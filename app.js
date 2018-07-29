c8fad32cd0631f84aedf537a482ca087b6c8b4cb
Mauricio Machado <macmauricio@gmail.com>
2018-07-29 15:28:57 -0300

    Alguna pagina un poco mas modelada - Ajax colocados

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
-$(document).ready(init);
+
+$(document).on('pageinit', init)
+
+$(document).on('pagebeforeshow', '#inicio', function(e, data){      
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/weather",
+        type: "GET",
+        dataType: "json",
+        data: {q: 'Montevideo', appid: "1573c2baa2cfe07cb8ee524834829651", lang: "es", units: "metric"},
+        success: function(response){
+            console.log("success",response);
+        },error: function(response){
+            console.log("error",response);
+            $("#mensaje").text("No hay nadie!");
+        }
+    });       
+});
+
+$(document).on('pagebeforeshow', '#medicos', function(e, data){      
+       
+});
+
+$(document).on('pagebeforeshow', '#centros', function(e, data){      
+      
+});
+
+$(document).on('pagebeforeshow', '#consultas', function(e, data){      
+       
+});
 
 function init() {
-    
+  
 }
